d15dc46a847c619b82fb606afc4a24906f15d2e7
AnilDhaka-l12 <anildhakal871@gmail.com>
2026-04-15 10:47:43 +0300

    ddhhd

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,30 @@
-const { TextEncoder, TextDecoder } = require('util')
-
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+module.exports = {
+  env: {
+    browser: true,
+    node: true,      // ← ADD THIS (recognizes require, global)
+    es2021: true,
+    jest: true       // ← ADD THIS (recognizes Jest globals)
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
