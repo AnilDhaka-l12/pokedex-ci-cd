447d0e9956fe793db3f57d6858ed9b22a21e697d
AnilDhaka-l12 <anildhakal871@gmail.com>
2026-04-19 02:56:34 +0300

    Rewrite app.js - clean version with semicolons

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const pokemon = require('./pokemon.json')
+const express = require('express');
+const path = require('path');
+const app = express();
+const pokemon = require('./pokemon.json');
 
-// Log all requests for debugging
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`)
-  next()
-})
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
-// API routes - MUST come BEFORE static files
 app.get('/api/pokemon', (req, res) => {
-  console.log('Sending all Pokemon')
-  res.json(pokemon)
-})
+  res.json(pokemon);
+});
 
 app.get('/api/pokemon/:name', (req, res) => {
-  const found = pokemon.find(p => p.name === req.params.name)
+  const found = pokemon.find(p => p.name === req.params.name);
   if (found) {
-    console.log(`Sending ${req.params.name}`)
-    res.json(found)
+    res.json(found);
   } else {
-    res.status(404).json({ error: 'Not found' })
+    res.status(404).json({ error: 'Not found' });
   }
-})
+});
 
-// Health check
 app.get('/health', (req, res) => {
-  console.log('Health check')
-  res.send('ok')
-})
+  res.send('ok');
+});
 
-// Version
 app.get('/version', (req, res) => {
-  console.log('Version check')
-  res.send('28')
-})
+  res.send('28');
+});
 
-// Serve static files from dist directory
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// For any other request, serve index.html
 app.get('*', (req, res) => {
-  console.log('Serving index.html')
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-module.exports = app
+module.exports = app;
