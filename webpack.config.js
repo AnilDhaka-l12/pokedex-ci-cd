978d0984a92d728c1bdceaa5e0006fb6e1dbd875
AnilDhaka-l12 <anildhakal871@gmail.com>
2026-04-18 22:35:15 +0300

    Fix Babel configuration for Render

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,13 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { node: 'current' } }],
+              ['@babel/preset-react', { runtime: 'automatic' }]
+            ]
+          }
+        }
       },
       {
         test: /\.css$/,
