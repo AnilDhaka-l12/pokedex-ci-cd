fcf3867c3fa53fc5115bf102445e5ffd350df187
AnilDhaka-l12 <anildhakal871@gmail.com>
2026-04-18 12:30:11 +0300

    Fix app.js formatting with single quotes

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-// Pokemon data directly in file (no external json file needed)
+// Pokemon data directly in file
 const pokemon = [
-  {"id": 1, "name": "bulbasaur", "type": ["grass", "poison"], "abilities": ["overgrow"]},
-  {"id": 2, "name": "ivysaur", "type": ["grass", "poison"], "abilities": ["overgrow"]},
-  {"id": 3, "name": "venusaur", "type": ["grass", "poison"], "abilities": ["overgrow"]},
-  {"id": 4, "name": "charmander", "type": ["fire"], "abilities": ["blaze"]},
-  {"id": 5, "name": "charmeleon", "type": ["fire"], "abilities": ["blaze"]},
-  {"id": 6, "name": "charizard", "type": ["fire", "flying"], "abilities": ["blaze"]},
-  {"id": 7, "name": "squirtle", "type": ["water"], "abilities": ["torrent"]},
-  {"id": 8, "name": "wartortle", "type": ["water"], "abilities": ["torrent"]},
-  {"id": 9, "name": "blastoise", "type": ["water"], "abilities": ["torrent"]},
-  {"id": 25, "name": "pikachu", "type": ["electric"], "abilities": ["static"]},
-  {"id": 133, "name": "eevee", "type": ["normal"], "abilities": ["run-away", "adaptability"]},
-  {"id": 150, "name": "mewtwo", "type": ["psychic"], "abilities": ["pressure"]},
-  {"id": 151, "name": "mew", "type": ["psychic"], "abilities": ["synchronize"]}
+  { 'id': 1, 'name': 'bulbasaur', 'type': ['grass', 'poison'], 'abilities': ['overgrow'] },
+  { 'id': 2, 'name': 'ivysaur', 'type': ['grass', 'poison'], 'abilities': ['overgrow'] },
+  { 'id': 3, 'name': 'venusaur', 'type': ['grass', 'poison'], 'abilities': ['overgrow'] },
+  { 'id': 4, 'name': 'charmander', 'type': ['fire'], 'abilities': ['blaze'] },
+  { 'id': 5, 'name': 'charmeleon', 'type': ['fire'], 'abilities': ['blaze'] },
+  { 'id': 6, 'name': 'charizard', 'type': ['fire', 'flying'], 'abilities': ['blaze'] },
+  { 'id': 7, 'name': 'squirtle', 'type': ['water'], 'abilities': ['torrent'] },
+  { 'id': 8, 'name': 'wartortle', 'type': ['water'], 'abilities': ['torrent'] },
+  { 'id': 9, 'name': 'blastoise', 'type': ['water'], 'abilities': ['torrent'] },
+  { 'id': 25, 'name': 'pikachu', 'type': ['electric'], 'abilities': ['static'] },
+  { 'id': 133, 'name': 'eevee', 'type': ['normal'], 'abilities': ['run-away', 'adaptability'] },
+  { 'id': 150, 'name': 'mewtwo', 'type': ['psychic'], 'abilities': ['pressure'] },
+  { 'id': 151, 'name': 'mew', 'type': ['psychic'], 'abilities': ['synchronize'] }
 ]
 
 console.log(`Loaded ${pokemon.length} Pokemon`)
